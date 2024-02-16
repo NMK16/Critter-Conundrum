637120809b5c940405c491c4503bee23f15cbf18
Michago6 <144434488+Michago6@users.noreply.github.com>
2024-02-16 15:37:59 +0100

    Add files via upload

--- Code/Project/Critter_Conundrum.ts
+++ Code/Project/Critter_Conundrum.ts
@@ -19,9 +19,61 @@ import * as BinaryTreeData from './animal_data.json';
 type Leaf = string;
 type Tree = {value: string, left: Tree, right: Tree} | Leaf;
 
+function add_new_animal(question: string, new_animal: string): void {
+	const fs = require('fs');
+	const fileName = './test.json';
+	const file = require(fileName);
+		
+	file.value = "";
+		
+	fs.writeFile(fileName, JSON.stringify(file, null, 2), function writeJSON(err) {
+	  if (err) return console.log(err);
+	  console.log(JSON.stringify(file, null, 2));
+	  console.log('writing to ' + fileName);
+	});
+}
+
+let turns: number = 0;
+let new_tree: Tree = {};
+/**
+ * 
+ * @param tree 
+ */
 function game_turn(tree: Tree) {
 	if(typeof(tree) === "string") {
-		console.log("Are you thinking of a " + tree + "?  (y/n)");
+		const readline = require('readline').createInterface({
+			input: process.stdin,
+			output: process.stdout
+		});
+		
+		readline.question("Are you thinking of a " + tree + " (y/n) -> ", userInput  => {
+			if(userInput === "y") {
+				readline.close();
+				console.log("I'm the best! I guessed your animal in " + turns + " questions!")
+			} else if (userInput === "n"){
+				//fill json file with new animal
+				const readline = require('readline').createInterface({
+					input: process.stdin,
+					output: process.stdout
+				});
+				console.log("Sorry but it seems that I don't know your animal.");
+				readline.question("What animal were you thinking of?", userInput_animal  => {
+					const readline = require('readline').createInterface({
+						input: process.stdin,
+						output: process.stdout
+					});
+					
+					readline.question("Give me a question that separates your animal from a " + tree + ", where your animal has the answer yes.", userInput_question  => {
+						add_new_animal(userInput_question, userInput_animal);
+					})
+
+				});
+			} else {
+				console.log("Wrong input");
+				readline.close();
+				game_turn(tree);
+			}
+		});
 	} else {
 		const readline = require('readline').createInterface({
 			input: process.stdin,
@@ -30,10 +82,13 @@ function game_turn(tree: Tree) {
 		
 		readline.question(tree.value + " (y/n) -> ", userInput  => {
 		if(userInput === "y"){
+			new_tree;
 			readline.close();
+			turns++;
 			game_turn(tree.right);
 		} else if(userInput === "n" ){
 			readline.close();
+			turns++;
 			game_turn(tree.left);
 		} else {
 			console.log("Wrong input");
@@ -43,5 +98,6 @@ function game_turn(tree: Tree) {
 		});
 	}
 }
+
 game_turn(BinaryTreeData);
 
