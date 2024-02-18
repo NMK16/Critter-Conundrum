968d8976b9e9b762c7e357227a40f579a8832878
NMK16 <144688680+NMK16@users.noreply.github.com>
2024-02-18 17:52:40 +0100

    Add files via upload
    Created a previous turn function and implemented it in the main function.

--- Code/Critter_Conundrum.ts
+++ Code/Critter_Conundrum.ts
@@ -1,11 +1,12 @@
-import { List, head, list, is_null, tail, append } from '../lib/list'
 import * as BinaryTreeData from './animal_data.json';
-
+import {List, head, list, is_null, tail, append } from './lib/list'
+import {Stack, pop, top, empty, push, NonEmptyStack, is_empty,} from './lib/stack'
 //to run: tsc Critter_Conundrum.ts --resolveJsonModule && node Critter_Conundrum.js
 
 type Leaf = string;
 type Tree = {value: string, left: Tree, right: Tree} | Leaf;
 type nonEmptyTree = {value: string, left: Tree, right: Tree};
+
 /**
  * 
  * @param path 
@@ -17,7 +18,7 @@ function edit_in_tree(path: List<string>, tree: Tree, new_input: nonEmptyTree): 
 	if(is_null(path)) {
 		return new_input;
 	} else if(head(path) === "right") {
-		if(typeof(tree) === "string") {
+		if(typeof(tree) === "string") { 
       console.log("error: premature leaf found");
 			return "error";
 		} else {
@@ -57,8 +58,23 @@ function add_new_animal(question: string, old_animal: string, new_animal: string
 	  console.log('writing to ' + fileName);
 	});
 }
+/**
+ * 
+ * @param tree 
+ */
+function go_back(tree: Tree): void {
+	if (!is_empty(game_history)){
+		turns--;
+		game_turn(top(game_history));
+		game_history = pop(game_history);
+	}else {
+	  console.log("You haven't made a move to undo yet.");
+	  game_turn(tree);
+	}
+}
 let path_to_animal: List<string> = list();
 let turns: number = 0;
+let game_history: Stack<Tree> = empty();
 /**
  * 
  * @param tree 
@@ -70,7 +86,7 @@ function game_turn(tree: Tree): void {
 			output: process.stdout
 		});
 		
-		readline.question("Are you thinking of a " + tree + " (y/n) -> ", userInput  => {
+		readline.question("Are you thinking of a " + tree + " (y/n/b) -> ", userInput  => {
 			readline.close();
 			if(userInput === "y") {
 				console.log("I'm the best! I guessed your animal in " + turns + " questions!")
@@ -97,9 +113,9 @@ function game_turn(tree: Tree): void {
 						readline3.close();
 						add_new_animal(userInput_question, tree, userInput_animal, path_to_animal);
 					});
-				});
-				
-
+				});	
+			} else if (userInput === "b") {
+				go_back(tree);
 			} else {
 				console.log("Wrong input");
 				game_turn(tree);
@@ -110,18 +126,22 @@ function game_turn(tree: Tree): void {
 			input: process.stdin,
 			output: process.stdout
 		});
-		
 		readline.question(tree.value + " (y/n) -> ", userInput  => {
 		if(userInput === "y"){
 			readline.close();
 			path_to_animal = append(path_to_animal, list("right"));
 			turns++;
+			game_history = push(tree, game_history);
 			game_turn(tree.right);
 		} else if(userInput === "n" ){
 			readline.close();
 			path_to_animal = append(path_to_animal, list("left"));
 			turns++;
+			game_history = push(tree, game_history);
 			game_turn(tree.left);
+		} else if (userInput === "b") {
+			readline.close();
+			go_back(tree);
 		} else {
 			console.log("Wrong input");
 			readline.close();
@@ -132,4 +152,3 @@ function game_turn(tree: Tree): void {
 }
 
 game_turn(BinaryTreeData);
-
